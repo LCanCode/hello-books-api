6fec517885be23fb3b9dfbf928b63c7bebc98de8
La Tasha Pollard <latashajpollard@gmail.com>
2023-05-10 12:50:38 -0500

    adds all of the api work

---
+++ app/author_routes.py
@@ -0,0 +1,60 @@
+from app import db
+from app.models.author import Author
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request, abort
+from app.book_routes import validate_model
+
+authors_bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
+
+@authors_bp.route("", methods=["POST"])
+def create_author():
+    request_body = request.get_json()
+    new_author = Author(name=request_body["name"],)
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return make_response(jsonify(f"Author {new_author.name} successfully created"), 201)
+
+#nested endpoint
+@authors_bp.route("/<author_id>/books", methods=["POST"])
+def create_book(author_id):
+    author = validate_model(Author, author_id)
+
+    request_body = request.get_json()
+    new_book = Book(
+        title=request_body["title"],
+        description=request_body["description"],
+        author=author
+    )
+
+    db.session.add(new_book)
+    db.session.commit()
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
+
+
+@authors_bp.route("", methods=["GET"])
+def get_all_authors():
+    
+    authors = Author.query.all()
+    authors_response = []
+    for author in authors:
+        authors_response.append({"name": author.name})
+    return jsonify(authors_response)
+
+#nested endpoint
+@authors_bp.route("/<author_id>/books", methods=["GET"])
+def get_books_by_author(author_id):
+    author = validate_model(Author, author_id)
+
+    books_response = []
+    for book in author.books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+
+        return jsonify(books_response)
