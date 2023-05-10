75e5218640943b16b4ac36aff5d9663a972231eb
La Tasha Pollard <latashajpollard@gmail.com>
2023-05-10 14:12:55 -0500

    updates app config to use render_database_uri

--- app/__init__.py
+++ app/__init__.py
@@ -13,14 +13,18 @@ def create_app(test_config=None):
 
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            # "SQLALCHEMY_DATABASE_URI")
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+            "RENDER_DATABASE_URI")
         
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        #     "SQLALCHEMY_TEST_DATABASE_URI")
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
+            "RENDER_DATABASE_URI")
     
     from app.models.book import Book
     from app.models.author import Author
